29d5f06c07cd42e344e2a0c6c08672dcffc35479
Sullivan <nope>
2019-07-01 15:09:16 +0200

    maj

--- hub2.php
+++ hub2.php
@@ -282,7 +282,7 @@ echo "<div style='width: 350px;margin-left: 640px'>";
 echo "<a href=\"archive.php\"><img src='img/Folder-Archive-icon.png' alt='' title='Archiver'/>Archiver</a>&emsp;&emsp;<a style='cursor: pointer' href='tacheSuppr.php'><img src=\"img/Programming-Show-Property-icon.png\" alt='Submit' title='Voir les tâches archivées'/>Voir les tâches archivées</a>";
 echo "</div>";
 
-echo "<div style='margin-right: 600px'>";
+echo "<div style='margin-left: 27px'>";
 echo "<form method='post' action='pdf.php' id='sub$cpt'><input type='text' name='order' value='$order' hidden><input type='text' name='orderD' value='$orderD' hidden><a style='float:bottom; cursor: pointer' onclick='valider($cpt)'><img src='img/Download-icon.png' alt='Submit' title='Imprimer en PDF'>Imprimer en PDF</a></form><br/>";
 echo "<br/>";
 echo "<br/>";
